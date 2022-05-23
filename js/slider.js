01090cc4afe5bc56b432cbe259f622d7a2fc825f
Eldar Akavov <akavoveldar3005@gmail.com>
2022-05-23 10:31:43 +0300

    add modal open and close func

--- js/slider.js
+++ js/slider.js
@@ -14,15 +14,44 @@ const swiper = new Swiper('.swiper', {
     disableOnInteraction: false,
   },
 });
-document.getElementById('call-popup').addEventListener('click', showDiv);
-let myDiv = true
-function showDiv(){
-  let isShow = document.getElementById('service');
-  if(myDiv){
-    isShow.style.display = 'none';
-    myDiv = false;
-  } else{
-    isShow.style.display = 'block';
-    myDiv = true;
+
+// document.getElementById('call-popup').addEventListener('click', showDiv);
+// let myDiv = true
+// function showDiv(){
+//   let isShow = document.getElementById('service');
+//   if(myDiv){
+//     isShow.style.display = 'none';
+//     myDiv = false;
+//   } else{
+//     isShow.style.display = 'block';
+//     myDiv = true;
+//   }
+// }
+// Получить модальный
+var modal = document.getElementById("myModal");
+
+// Получить кнопку, которая открывает модальный
+var btn = document.getElementById("myBtn");
+var btnHeader = document.getElementById("myBtnHeader");
+
+// Получить элемент <span>, который закрывает модальный
+var span = document.getElementById("myClose");
+
+// Когда пользователь нажимает на кнопку, откройте модальный
+btn.onclick = function() {
+  modal.style.display = "block";    
+}
+btnHeader.onclick = function() {
+  modal.style.display = "block";    
+}
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
