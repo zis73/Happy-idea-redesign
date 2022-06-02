7a209b30ca68c8476cfc2d6a581f23ef0adbbcc3
Eldar Akavov <akavoveldar3005@gmail.com>
2022-06-02 04:08:12 +0300

    add PHPMailer

---
+++ send.php
@@ -0,0 +1,54 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+//Load Composer's autoloader
+// require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$city = $_POST['city'];
+$message = $_POST['message'];
+$title = "Новая заявка";
+$body = "
+<h2>Новая заявка</h2>
+<b>Имя:</b>  $name<br>
+<b>Телефон:</b>  $phone<br>
+<b>Город:</b> $city<br>
+<b>Сообщение:</b> $message";
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->CharSet = "UTF-8";
+    $mail->Host       = 'smtp.mail.ru';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'h.idea@mail.ru';                     //SMTP username
+    $mail->Password   = 'cgQXbFtpjnbqZOa5nkTd';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('h.idea@mail.ru', 'Happy Idea');
+    $mail->addAddress('h.idea@mail.ru');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $title;
+    $mail->Body    = $body;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
