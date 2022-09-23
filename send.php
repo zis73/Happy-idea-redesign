7f631b9decd9b5a939d8e48d5260f80841134344
Bodrov Sergey <bodrov.sergey.d@gmail.com>
2022-09-23 20:45:44 +0300

    add capcha to index html

--- send.php
+++ send.php
@@ -25,32 +25,42 @@ $body = "
 <b>Телефон:</b>  $phone<br>
 <b>Город:</b> $city<br>
 <b>Сообщение:</b> $message";
-try {
-    //Server settings
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->CharSet = "UTF-8";
-    $mail->Host       = 'smtp.mail.ru';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'h.idea@mail.ru';                     //SMTP username
-    $mail->Password   = 'cgQXbFtpjnbqZOa5nkTd';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('h.idea@mail.ru', 'Happy Idea');
-    $mail->addAddress('h.idea@mail.ru');     //Add a recipient
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $title;
-    $mail->Body    = $body;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+if (isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
+	$secret = '6Ld9wvogAAAAABZiFDBtqZW4YKETMsBzDcIkxlbR';
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$response = $_POST['g-recaptcha-response'];
+	$rsp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$ip");
+	$arr = json_decode($rsp, TRUE);
+	if ($arr['success']) {
+		try {
+            //Server settings
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->CharSet = "UTF-8";
+            $mail->Host       = 'smtp.mail.ru';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'h.idea@mail.ru';                     //SMTP username
+            $mail->Password   = 'cgQXbFtpjnbqZOa5nkTd';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        
+            //Recipients
+            $mail->setFrom('h.idea@mail.ru', 'Happy Idea');
+            $mail->addAddress('h.idea@mail.ru');     //Add a recipient
+        
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = $title;
+            $mail->Body    = $body;
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+	}
 }
+
 
 header('Location: thanks.html');
